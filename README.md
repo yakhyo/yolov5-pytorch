9db89fe2825af62980ebc7b9ba5e1fd3adaec4d9
yakhyo <yakhyo9696@gmail.com>
2022-01-06 11:37:58 +0900

    Update

---
+++ README.md
@@ -0,0 +1,55 @@
+## YOLOv5 models
+
+`yolov5.py`:
+
+```python
+import torch
+from common import YOLOv5
+
+_nc = 80
+
+_anchors = [
+    [10, 13, 16, 30, 33, 23],  # P3/8
+    [30, 61, 62, 45, 59, 119],  # P4/16
+    [116, 90, 156, 198, 373, 326]  # P5/32
+]
+
+_conf = {'n': (0.33, 0.25),
+         's': (0.33, 0.5),
+         'm': (0.67, 0.75),
+         'l': (1.0, 1.0),
+         'x': (1.33, 1.25)}
+
+
+def yolov5(nc, anchors, conf):
+    return YOLOv5(nc, anchors, conf)
+
+
+def out(net):
+    net.eval()
+    img = torch.randn(1, 3, 640, 640)
+    prediction, (p3, p4, p5) = net(img)
+    return f'\nP3: {p3.shape}, \nP4: {p4.shape}, \nP5: {p5.shape}'
+
+
+if __name__ == '__main__':
+    yolov5n = yolov5(_nc, _anchors, _conf['n'])
+    yolov5s = yolov5(_nc, _anchors, _conf['s'])
+    yolov5m = yolov5(_nc, _anchors, _conf['m'])
+    yolov5l = yolov5(_nc, _anchors, _conf['l'])
+    yolov5x = yolov5(_nc, _anchors, _conf['x'])
+
+    print("YOLOv5n params.: {:0.2f}M".format(sum(p.numel() for p in yolov5n.parameters() if p.requires_grad) / 1e6))
+    print("YOLOv5s params.: {:0.2f}M".format(sum(p.numel() for p in yolov5s.parameters() if p.requires_grad) / 1e6))
+    print("YOLOv5m params.: {:0.2f}M".format(sum(p.numel() for p in yolov5m.parameters() if p.requires_grad) / 1e6))
+    print("YOLOv5l params.: {:0.2f}M".format(sum(p.numel() for p in yolov5l.parameters() if p.requires_grad) / 1e6))
+    print("YOLOv5x params.: {:0.2f}M".format(sum(p.numel() for p in yolov5x.parameters() if p.requires_grad) / 1e6))
+```
+Output
+```
+    YOLOv5n params.: 1.87M
+    YOLOv5s params.: 7.24M
+    YOLOv5m params.: 21.19M
+    YOLOv5l params.: 46.56M
+    YOLOv5x params.: 86.75M
+```
--- models/common.py
+++ models/common.py
@@ -3,22 +3,18 @@ import math
 import torch
 import torch.nn as nn
 
-anchors = [
-    [10, 13, 16, 30, 33, 23],  # P3/8
-    [30, 61, 62, 45, 59, 119],  # P4/16
-    [116, 90, 156, 198, 373, 326]]  # P5/32
-
 
 # Pad to 'same'
 def _pad(k, p=None):
     if p is None:
-        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
 
 
 # Standard convolution
 class Conv(nn.Module):
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, _pad(k, p), groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
@@ -31,9 +27,10 @@ class Conv(nn.Module):
         return self.act(self.conv(x))
 
 
-# Standard bottleneck
+# Residual bottleneck
 class Bottleneck(nn.Module):
-    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):
         super().__init__()
         c_ = int(c2 * e)
         self.conv1 = Conv(c1, c_, 1, 1)
@@ -46,9 +43,10 @@ class Bottleneck(nn.Module):
 
 # CSP Bottleneck with 3 convolutions
 class C3(nn.Module):
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
         super().__init__()
-        c_ = int(c2 * e)  # hidden channels
+        c_ = int(c2 * e)
         self.conv1 = Conv(c1, c_, 1, 1)
         self.conv2 = Conv(c1, c_, 1, 1)
         self.conv3 = Conv(2 * c_, c2, 1)
@@ -58,115 +56,121 @@ class C3(nn.Module):
         return self.conv3(torch.cat((self.m(self.conv1(x)), self.conv2(x)), dim=1))
 
 
-# Spatial Pyramid Pooling (SPP) layer [https://arxiv.org/abs/1406.4729]
-class SPP(nn.Module):
-    def __init__(self, c1, c2, k=(5, 9, 13)):
+# Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
+class SPPF(nn.Module):
+
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
         super().__init__()
         c_ = c1 // 2  # hidden channels
-        self.conv1 = Conv(c1, c_, 1, 1)
-        self.conv2 = Conv(c_ * (len(k) + 1), c2, 1, 1)
-        self.m = nn.ModuleList([nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
-
-    def forward(self, x):
-        x = self.conv1(x)
-        return self.conv2(torch.cat([x] + [m(x) for m in self.m], 1))
-
-
-# Spatial Pyramid Pooling - Fast (SPPF)
-class SPPF(nn.Module):
-    def __init__(self, c1, c2, k=5):
-        super().__init__()
-        c_ = c1 // 2
         self.conv1 = Conv(c1, c_, 1, 1)
         self.conv2 = Conv(c_ * 4, c2, 1, 1)
         self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
 
     def forward(self, x):
         x = self.conv1(x)
+
         y1 = self.m(x)
         y2 = self.m(y1)
         return self.conv2(torch.cat([x, y1, y2, self.m(y2)], 1))
 
 
-class DarkNet(nn.Module):
-    def __init__(self):
-        super(DarkNet, self).__init__()
-        self.b0 = Conv(3, 64, 6, 2, 2)  # 0-P1/2
-        self.b1 = Conv(64, 128, 3, 2)  # 1-P2/4
-        self.b2 = C3(128, 128, 3)  # 2
-        self.b3 = Conv(128, 256, 3, 2)  # 3-P3/8
-        self.b4 = C3(256, 256, 6)  # 4
-        self.b5 = Conv(256, 512, 3, 2)  # 5-P4/16
-        self.b6 = C3(512, 512, 9)  # 6
-        self.b7 = Conv(512, 1024, 3, 2)  # 7-P5/32
-        self.b8 = C3(1024, 1024, 3)  # 8
-        self.b9 = SPPF(1024, 1024, 5)  # 9
+# Concatenating list of layers
+class Concat(nn.Module):
+
+    def __init__(self, d=1):
+        super().__init__()
+        self.d = d
+
+    def __call__(self, x: list):
+        return torch.cat(x, self.d)
+
+
+# Backbone
+class BACKBONE(nn.Module):
+
+    def __init__(self, filters, depths):
+        super().__init__()
+
+        self.b0 = Conv(filters[0], filters[1], 6, 2, 2)  # 0-P1/2
+        self.b1 = Conv(filters[1], filters[2], 3, 2)  # 1-P2/4
+        self.b2 = C3(filters[2], filters[2], depths[0])
+        self.b3 = Conv(filters[2], filters[3], 3, 2)  # 3-P3/8
+        self.b4 = C3(filters[3], filters[3], depths[1])
+        self.b5 = Conv(filters[3], filters[4], 3, 2)  # 5-P4/16
+        self.b6 = C3(filters[4], filters[4], depths[2])
+        self.b7 = Conv(filters[4], filters[5], 3, 2)  # 7-P5/32
+        self.b8 = C3(filters[5], filters[5], depths[0])
+        self.b9 = SPPF(filters[5], filters[5], 5)  # 9
 
     def forward(self, x):
-        b0 = self.b0(x)
-        b1 = self.b1(b0)
+        b0 = self.b0(x)  # 0-P1/2
+        b1 = self.b1(b0)  # 1-P2/4
         b2 = self.b2(b1)
-        b3 = self.b3(b2)
+        b3 = self.b3(b2)  # 3-P3/8
         b4 = self.b4(b3)
-        b5 = self.b5(b4)
+        b5 = self.b5(b4)  # 5-P4/16
         b6 = self.b6(b5)
-        b7 = self.b7(b6)
+        b7 = self.b7(b6)  # 7-P5/32
         b8 = self.b8(b7)
         b9 = self.b9(b8)
 
         return b4, b6, b9
 
 
-class Head(nn.Module):
-    def __init__(self):
-        super(Head, self).__init__()
-        self.h10 = Conv(1024, 512, 1, 1)  # 10
-        self.h11 = nn.Upsample(None, scale_factor=2, mode='nearest')  # 11
-        # self.h12 cat backbone P4 # 12
-        self.h13 = C3(1024, 512, 3, shortcut=False)  # 13
+# Head
+class HEAD(nn.Module):
 
-        self.h14 = Conv(512, 256, 1, 1)  # 14
-        self.h15 = nn.Upsample(None, scale_factor=2, mode='nearest')  # 15
-        # self.h16 cat backbone P3 # 16
-        self.h17 = C3(512, 256, 3, shortcut=False)  # 17 (P3/8-small)
+    def __init__(self, filters, depths):
+        super().__init__()
 
-        self.h18 = Conv(256, 256, 3, 2)  # 18
-        # self.19 cat head P4 # 19
-        self.h20 = C3(512, 512, 3, shortcut=False)  # 20 (P4/16-medium)
+        self.h10 = Conv(filters[5], filters[4], 1, 1)
+        self.h11 = nn.Upsample(None, scale_factor=2, mode='nearest')
+        self.h12 = Concat()  # cat backbone P4
+        self.h13 = C3(filters[5], filters[4], depths[0], shortcut=False)  # 13
 
-        self.h21 = Conv(512, 512, 3, 2)  # 21
-        # self.h22 cat head P5 # 22
-        self.h23 = C3(1024, 10243, shortcut=False)  # 23 23 (P5/32-large)
+        self.h14 = Conv(filters[4], filters[3], 1, 1)
+        self.h15 = nn.Upsample(None, scale_factor=2, mode='nearest')
+        self.h16 = Concat()  # cat backbone P3
+        self.h17 = C3(filters[4], filters[3], depths[0], shortcut=False)  # 17 (P3/8-small)
+
+        self.h18 = Conv(filters[3], filters[3], 3, 2)
+        self.h19 = Concat()  # cat head P4
+        self.h20 = C3(filters[4], filters[4], depths[0], shortcut=False)  # 20 (P4/16-medium)
+
+        self.h21 = Conv(filters[4], filters[4], 3, 2)
+        self.h22 = Concat()  # cat head P5
+        self.h23 = C3(filters[5], filters[5], depths[0], shortcut=False)  # 23 (P5/32-large)
 
     def forward(self, x):
         p3, p4, p5 = x
 
         h10 = self.h10(p5)
         h11 = self.h11(h10)
-        h12 = torch.cat([h11, p4], dim=1)  # cat backbone P4
+        h12 = self.h12([h11, p4])  # cat backbone P4
         h13 = self.h13(h12)  # 13
 
         h14 = self.h14(h13)
         h15 = self.h15(h14)
-        h16 = torch.cat([h15, p3], dim=1)  # cat backbone P3
-        h17 = self.h17(h16)
+        h16 = self.h16([h15, p3])  # cat backbone P3
+        h17 = self.h17(h16)  # 17 (P3/8-small)
 
         h18 = self.h18(h17)
-        h19 = torch.cat([h18, h14], dim=1)  # cat head P4
+        h19 = self.h19([h18, h14])  # cat head P4
         h20 = self.h20(h19)  # 20 (P4/16-medium)
 
         h21 = self.h21(h20)
-        h22 = torch.cat([h21, h10], dim=1)  # cat head P5
+        h22 = self.h22([h21, h10])  # cat head P5
         h23 = self.h23(h22)  # 23 (P5/32-large)
 
         return h17, h20, h23
 
 
-class Detect(nn.Module):
+# Detection Head
+class DETECT(nn.Module):
     stride = None  # strides computed during build
     onnx_dynamic = False  # ONNX export parameter
 
-    def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
+    def __init__(self, nc=80, anchors=(), ch=()):  # detection layer
         super().__init__()
         self.nc = nc  # number of classes
         self.no = nc + 5  # number of outputs per anchor
@@ -176,7 +180,6 @@ class Detect(nn.Module):
         self.anchor_grid = [torch.zeros(1)] * self.nl  # init anchor grid
         self.register_buffer('anchors', torch.tensor(anchors).float().view(self.nl, -1, 2))  # shape(nl,na,2)
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
-        self.inplace = inplace  # use in-place ops (e.g. slice assignment)
 
     def forward(self, x):
         z = []  # inference output
@@ -207,13 +210,22 @@ class Detect(nn.Module):
         return grid, anchor_grid
 
 
+# Model
 class YOLOv5(nn.Module):
-    def __init__(self, anchors):
+
+    def __init__(self, nc, anchors, conf):
         super(YOLOv5, self).__init__()
 
-        self.backbone = DarkNet()
-        self.head = Head()
-        self.detect = Detect(anchors=anchors, ch=(1024, 512, 256))
+        filters = [3, 64, 128, 256, 512, 1024]
+        depths = [3, 6, 9]
+        depth_multiple, width_multiple = conf
+
+        depths = [max(round(n * depth_multiple), 1) for n in depths]
+        filters = [3, *[self._make_divisible(c * width_multiple, 8) for c in filters[1:]]]
+
+        self.backbone = BACKBONE(filters, depths)
+        self.head = HEAD(filters, depths)
+        self.detect = DETECT(nc=nc, anchors=anchors, ch=(filters[3], filters[4], filters[5]))
 
         dummy_img = torch.zeros(1, 3, 256, 256)
         self.detect.stride = torch.tensor([256 / x.shape[-2] for x in self.forward(dummy_img)])
@@ -225,6 +237,13 @@ class YOLOv5(nn.Module):
         p3, p4, p5 = self.backbone(x)
         p3, p4, p5 = self.head([p3, p4, p5])
         return self.detect([p3, p4, p5])
+
+    @staticmethod
+    def _make_divisible(x, divisor):
+        # Returns nearest x divisible by divisor
+        if isinstance(divisor, torch.Tensor):
+            divisor = int(divisor.max())  # to int
+        return math.ceil(x / divisor) * divisor
 
     @staticmethod
     def _initialize_biases(detect):
@@ -242,12 +261,3 @@ class YOLOv5(nn.Module):
         ds = det.stride[-1] - det.stride[0]  # delta s
         if da.sign() != ds.sign():  # same order
             det.anchors[:] = det.anchors.flip(0)
-
-
-if __name__ == '__main__':
-    net = YOLOv5(anchors=anchors)
-    # net.eval() # error occurs, fixing...
-    img = torch.randn(1, 3, 640, 640)
-    p3, p4, p5 = net(img)
-    print(p3.shape, p4.shape, p5.shape)
-    print("Num. of parameters: {}".format(sum(p.numel() for p in net.parameters() if p.requires_grad)))
